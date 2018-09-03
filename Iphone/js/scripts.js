ac3d63badd5a46796f4c9426047dc0343a12c4f8
Aleksandr <semenovaleksandr1983@yandex.ru>
2018-09-03 19:57:34 +0300

    second commit

---
+++ Iphone/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+		$('.slider_slick').slick({
+			infinite: true,
+			slidesToShow: 3,
+			arrows: false,
+			dots: true,
+			centerMode: true,
+			centerPadding: '0px'
+		});
+	});	
+jQuery(document).ready(function($) {
+		$('.my-slider').unslider({
+			autoplay:true,
+			arrows:false,
+			delay: 100000000
+		});
+	});
---
+++ LeoShop/js/scripts.js
@@ -0,0 +1,8 @@
+$(function () {
+	$('.search').on('click', function () {
+		$('.navbar-form').slideToggle();
+	});
+	$('.carousel').carousel({
+		interval: false
+	});
+});
