c948f620a7c552f18aaee82f58a37464711c77b9
Aleksandr <semenovaleksandr1983@yandex.ru>
2018-10-20 00:14:52 +0300

    20.10.18

---
+++ Portfolio/js/common.js
@@ -0,0 +1,44 @@
+$(function() {
+
+	$('#AdobePhotoshop').LineProgressbar({
+		percentage:80,
+		radius: '3px',
+		height: '10px',
+		fillBackgroundColor: '#ffae00'
+	});
+	$('#jQuery').LineProgressbar({
+		percentage:70,
+		radius: '3px',
+		height: '10px',
+		fillBackgroundColor: '#ffae00'
+	});
+	$('#Html5Css3').LineProgressbar({
+		percentage:89,
+		radius: '3px',
+		height: '10px',
+		fillBackgroundColor: '#ffae00'
+	});
+	$('#SASSLESS').LineProgressbar({
+		percentage:87,
+		radius: '3px',
+		height: '10px',
+		fillBackgroundColor: '#ffae00'
+	});
+	$('#Bootstrap').LineProgressbar({
+		percentage:90,
+		radius: '3px',
+		height: '10px',
+		fillBackgroundColor: '#ffae00'
+	});
+	$('data-fancybox').fancybox({
+		buttons: [
+    "zoom",
+    //"share",
+    "slideShow",
+    //"fullScreen",
+    //"download",
+    "thumbs",
+    "close"
+  ],
+	});
+});
